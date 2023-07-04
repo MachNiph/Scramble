cbd3c1c3161590ec4eaa27f2098920c0a213b549
redshirtfull456 <nishanphuyal111@gmail.com>
2023-07-04 08:22:00 +0545

    HintAdd

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const values = [
-  "react",
-  "javascript",
-  "index",
-  "script",
-  "unity",
-  "component",
-  "render",
-  "transform",
-  "rotation",
-  "java",
-  "dotnet",
+  {
+    name: "react",
+    hint: "Hint: javaScript library",
+  },
+  {
+    name: "javascript",
+    hint: " Hint: scripting language",
+  },
+  {
+    name: "unity",
+    hint: "Hint: 2D/3D engine and framework",
+  },
+  {
+    name: "flutter",
+    hint: "Hint: Google's portable UI toolkit",
+  },
+  {
+    name: "java",
+    hint: "Hint: multi-platform, object-oriented, and network-centric language ",
+  },
 ];
 const guessWord = document.querySelector(".guess-word");
 const inputWord = document.querySelector(".guesshere");
 const guessBtn = document.querySelector(".guess");
 const playAgainBtn = document.querySelector(".playagain");
 const result = document.querySelector(".result");
+const hintShow = document.querySelector(".hintshow");
+const hintBtn = document.querySelector(".hintbtn");
 
 let currentWordIndex = 0;
 scramble();
 
 function scramble() {
   let inputText;
-  const word = values[currentWordIndex];
+  const word = values[currentWordIndex].name;
   const strarray = word.split("");
   let i, j, k;
   for (i = 0; i < strarray.length; i++) {
@@ -42,11 +53,15 @@ function scramble() {
     location.reload();
   });
 
+  hintBtn.addEventListener("click", () => {
+    hintShow.innerHTML = values[currentWordIndex].hint;
+  });
+
   guessBtn.addEventListener("click", () => {
     if (inputText == word) {
       result.innerHTML = "You Guessed Right!!";
       inputWord.value = "";
-
+      hintShow.innerHTML = "";
       currentWordIndex++;
       if (currentWordIndex < values.length) {
         scramble();
